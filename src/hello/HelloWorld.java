eabf2c633e1c9124720284c9887378d5e5be15de
Mazen Kasher <mazenkasher17@gmail.com>
2022-08-24 12:35:14 -0400

    we shared project

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
         // 2. add comint
+        // we shared project
+
     }
 }
